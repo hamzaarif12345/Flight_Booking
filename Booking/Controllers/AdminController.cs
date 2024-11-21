90a12ad3e60285d348b719ce0d66abaf7cd8fd83
Hamza <hamza81611@gmail.com>
2024-11-21 15:33:18 +0530

    adding the code

---
+++ Booking/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Booking;
+
+
+namespace FlightBookingSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AirlineEntities _context;
+
+        public AdminController()
+        {
+            _context = new AirlineEntities();
+        }
+
+        // View all flights
+        public ActionResult ManageFlights()
+        {
+            var flights = _context.Flights.ToList();
+            return View(flights);
+        }
+
+        // View all users
+        public ActionResult ManageUsers()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+
+        // View all tickets
+        public ActionResult ManageTickets()
+        {
+            var tickets = _context.Tickets.ToList();
+            return View(tickets);
+        }
+
+        // Dispose the context
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Booking/Controllers/FlightsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Booking.Controllers
+{
+    public class FlightsController : Controller
+    {
+        private readonly AirlineEntities _db = new AirlineEntities();
+
+        public ActionResult Index()
+        {
+            var flights = _db.Flights.ToList();
+            return View(flights);
+        }
+
+        // Add Edit and Delete actions here as required.
+        public ActionResult Edit(int id)
+        {
+            var flight = _db.Flights.Find(id);
+            if (flight == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flight);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Flight flight)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(flight).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(flight);
+        }
+        public ActionResult Delete(int id)
+        {
+            var flight = _db.Flights.Find(id);
+            if (flight == null)
+            {
+                return HttpNotFound();
+            }
+            _db.Flights.Remove(flight);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
